02352cd6e57f9bf9bd44c2b16c6e65ff9f83ce74
Michael Simire <simiremichael@gmail.com>
2025-07-11 06:39:01 +0100

    updated

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -65,7 +65,10 @@ function Footer({ pageTitle }: any) {
                 <path d="M389.2 48h70.6L305.6 224.2 487 464H345L233.7 318.6 106.5 464H35.8L200.7 275.5 26.8 48H172.4L272.9 180.9 389.2 48zM364.4 421.8h39.1L151.1 88h-42L364.4 421.8z" />
               </svg>
             </a>
-            <a className="social-icons mr-5 cursor-pointer">
+            <a
+              className="social-icons mr-5 cursor-pointer"
+              href="https://web.facebook.com/profile.php?id=61577974693738#"
+            >
               <svg
                 width={19}
                 xmlns="http://www.w3.org/2000/svg"
